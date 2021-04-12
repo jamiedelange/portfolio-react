132baaa5451da0b601de391a11305e7f07dc9a8a
jelly octane <jamie.de.lange@mac.com>
2021-04-12 12:40:46 -0500

    push

---
+++ src/components/Contact/index 2.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
+
+function ContactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+    const [errorMessage, setErrorMessage] = useState('');
+    function handleChange(e) {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            if (!isValid) {
+                setErrorMessage('your email is invalid.');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+        if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value });
+        }
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+
+    return (
+        <section className="form-box">
+            <h2 className="contact-header">E-mail me at <a href="mailto:jamie.de.lange@mac.com">jamie.de.lange@mac.com</a></h2>
+            <h3 className="contact-header">... or leave a note:</h3>
+            <form id="contact-form" onSubmit={handleSubmit}>
+                <div className="contact-field-div">
+                    <label htmlFor="name">Name: </label>
+                    <input type="text" defaultValue={name} onBlur={handleChange} name="name"></input>
+                </div>
+                <div className="contact-field-div">
+                    <label htmlFor="email">E-mail address: </label>
+                    <input type="email" defaultValue={email} onBlur={handleChange} name="email"></input>
+                </div>
+                <div className="contact-field-div">
+                    <label htmlFor="message">Message: </label>
+                    <textarea name="message" defaultValue={message} onBlur={handleChange} rows="5"></textarea>
+                </div>
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
+                <button type="submit">Submit</button>
+            </form>
+        </section>
+    )
+}
+
+export default ContactForm;
---
+++ src/components/Portfolio/index 2.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import ss1 from '../../assets/1.png'
+import ss2 from '../../assets/2.png'
+import ss3 from '../../assets/3.png'
+import ss4 from '../../assets/4.png'
+import ss5 from '../../assets/5.png'
+import ss6 from '../../assets/6.png'
+import githubIcon from '../../assets/github-icon.png';
+
+function Portfolio() {
+    return (
+        <section className="portfolio-box">
+            <div className="portfolio-header">
+                <h2>Portfolio</h2>
+            </div>
+            <div className="grid">
+                <div className="grid-row">
+                    <div className="grid-item">
+                        <a href="https://jamiedelange.github.io/3D-hike/">
+                            <img className="img-thumbnail" src={`${ss1}`}></img>
+                        </a>
+                        <div className="grid-text">
+                            <a href="https://jamiedelange.github.io/3D-hike/">
+                                <h3>3D-Hike</h3>
+                            </a>
+                            <a  href="https://github.com/jamiedelange/3D-hike">
+                                <img className="icon-p" src={`${githubIcon}`} alt="github icon"></img>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="grid-item">
+                        <a href="https://guarded-harbor-93701.herokuapp.com">
+                            <img className="img-thumbnail" src={`${ss2}`}></img>
+                        </a>
+                        <div className="grid-text">
+                            <a href="https://guarded-harbor-93701.herokuapp.com">
+                                <h3>Tech Blog</h3>
+                            </a>
+                            <a href="https://github.com/jamiedelange/tech-blog">
+                                <img className="icon-p" src={`${githubIcon}`} alt="github icon"></img>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="grid-row">
+                    <div className="grid-item">
+                        <a href="https://hidden-shelf-19065.herokuapp.com/">
+                            <img className="img-thumbnail" src={`${ss5}`}></img>
+                        </a>
+                        <div className="grid-text">
+                            <a href="https://hidden-shelf-19065.herokuapp.com/">
+                                <h3>Budget Tracker</h3>
+                            </a>
+                            <a href="https://github.com/jamiedelange/budget-tracker">
+                                <img className="icon-p" src={`${githubIcon}`} alt="github icon"></img>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="grid-item">
+                        <a href="https://gentle-hollows-60962.herokuapp.com/login">
+                            <img className="img-thumbnail" src={`${ss4}`}></img>
+                        </a>
+                        <div className="grid-text">
+                            <a href="https://gentle-hollows-60962.herokuapp.com/login">
+                            <h3>Doorman</h3>
+                            </a>
+                            <a href="https://github.com/jamiedelange/Doorman">
+                                <img className="icon-p" src={`${githubIcon}`} alt="github icon"></img>
+                            </a>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div className="grid-row">
+                    <div className="grid-item">
+                        <a href="http://note-wizard.herokuapp.com/">
+                            <img className="img-thumbnail" src={`${ss3}`}></img>
+                        </a>
+                        <div className="grid-text">
+                            <a href="http://note-wizard.herokuapp.com/">
+                                <h3>Note Wizard</h3>
+                            </a>
+                            <a href="https://github.com/jamiedelange/note-wizard">
+                                <img className="icon-p" src={`${githubIcon}`} alt="github icon"></img>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Portfolio;
