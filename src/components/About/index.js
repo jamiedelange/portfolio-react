412c4c8e41775ecaf0f919680143d38a4756e16f
jelly octane <jamie.de.lange@mac.com>
2021-04-15 16:01:12 -0500

    push before update

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -11,6 +11,7 @@ function About() {
             <div className="summary-box">
                 <p>Hello! I am a full stack developer based in Nashville, Tennessee. I have a background in geology and music. 
                     I am a graduate of the University of Georgia, and I have recently acquired a certificate in full stack web development from Vanderbilt University.
+                    I am seeking an internship in full stack, front end or back end development. I am a dedicated worker passionate about producing functional applications of the highest possible quality.
                     When I'm not coding you can find me playing music or riding my bike. Thanks for visiting my page.
                 </p>
             </div>
