bab73ec64a5ddbe3119acbdac56047a1478a8c22
jelly octane <jamie.de.lange@mac.com>
2021-05-03 14:36:51 -0500

    edit

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,10 +9,10 @@ function About() {
         <div className="summary-column">
         <h3 id="about1">Hey, I'm Jamie. Thank <br/>you for visiting my page!</h3>
             <p className="summary">
-            I am a full stack developer based in Nashville, Tennessee. I
-            have a background in geology and music. I am a graduate of the
+            I am a full stack developer based in Nashville, Tennessee, with 
+            a background in geology and music. I am a graduate of the
             University of Georgia, and I have recently acquired a certificate in
-            full stack web development from Vanderbilt University. I am seeking an
+            full stack web development from Vanderbilt University. I'm seeking an
             internship in full stack, front end or back end development. I am a
             dedicated worker passionate about producing functional applications of
             the highest possible quality. When I'm not coding you can find me
@@ -21,7 +21,7 @@ function About() {
         </div>
         <div className="portrait-column">
             <img src={portrait} className="portrait" alt="portrait" />
-            <h3 className="contact-header">E-mail me at <a className="email-link" href="mailto:jamie.de.lange@mac.com">jamie.de.lange@mac.com</a></h3>
+            <h3 className="contact-header">Let's chat! E-mail me at <a className="email-link" href="mailto:jamie.de.lange@mac.com">jamie.de.lange@mac.com</a></h3>
         </div>
       </div>
     </div>
